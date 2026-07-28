169ec2251cf37adf2d3be60ae3dd0628b004647c
gilmaro6 <gilanmarsa@gmail.com>
2026-07-28 12:36:05 -0500

    Add example of string literals in Python

---
+++ seccion2/literales_cadenas.py
@@ -0,0 +1,18 @@
+# Ejemplo de literales de cadenas en Python
+
+nombre = "Gilmaro"
+ciudad = "Colombia"
+
+mensaje = "Estoy aprendiendo fundamentos de Python"
+
+print("Nombre:", nombre)
+print("Ciudad:", ciudad)
+print(mensaje)
+
+# Diferentes formas de escribir cadenas
+
+cadena_simple = 'Python es sencillo'
+cadena_doble = "Python es poderoso"
+
+print(cadena_simple)
+print(cadena_doble)
