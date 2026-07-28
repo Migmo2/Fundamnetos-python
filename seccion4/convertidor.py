2c3181e02c50679683a8e4f3417d84ea3b0c087d
gilmaro6 <gilanmarsa@gmail.com>
2026-07-28 12:41:29 -0500

    Add simple temperature converter in Python

---
+++ seccion4/convertidor.py
@@ -0,0 +1,8 @@
+# Convertidor simple de temperatura
+
+celsius = 25
+
+fahrenheit = (celsius * 9/5) + 32
+
+print("Temperatura en Celsius:", celsius)
+print("Temperatura en Fahrenheit:", fahrenheit)
