d0bbed4483d404937f899adbd67070a071f3a082
gilmaro6 <gilanmarsa@gmail.com>
2026-07-28 12:37:03 -0500

    Add basic mathematical operations in Python

---
+++ seccion3/operadores_matematicos.py
@@ -0,0 +1,23 @@
+# Operadores matemáticos en Python
+
+numero1 = 20
+numero2 = 6
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+division_entera = numero1 // numero2
+modulo = numero1 % numero2
+potencia = numero1 ** numero2
+
+print("Número 1:", numero1)
+print("Número 2:", numero2)
+
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
+print("División entera:", division_entera)
+print("Módulo:", modulo)
+print("Potencia:", potencia)
