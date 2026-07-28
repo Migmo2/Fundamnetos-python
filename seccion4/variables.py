78c365470f8b89b0983f07fd756b1869858ae1cc
gilmaro6 <gilanmarsa@gmail.com>
2026-07-28 12:39:51 -0500

    Add variables and print statements in Python

---
+++ seccion4/variables.py
@@ -0,0 +1,17 @@
+# Trabajo con variables en Python
+
+nombre = "Gilmaro"
+edad = 25
+altura = 1.75
+estudiante = True
+
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Altura:", altura)
+print("¿Es estudiante?", estudiante)
+
+# Modificando el valor de una variable
+
+edad = edad + 1
+
+print("Nueva edad:", edad)
